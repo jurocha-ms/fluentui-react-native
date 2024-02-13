6efcea2fca0741840a11ee31fc0a200f1dbff10b
Julio C. Rocha <julio.rocha@microsoft.com>
2024-02-12 17:13:51 -0800

    Create ActivityIndicator.macos.tsx

--- packages/experimental/ActivityIndicator/src/ActivityIndicator.tsx
+++ packages/experimental/ActivityIndicator/src/ActivityIndicator.tsx
@@ -1,3 +1,7 @@
-import { ActivityIndicator } from './CoreActivityIndicator';
-export { ActivityIndicator } from './CoreActivityIndicator';
+// import { ActivityIndicator } from './CoreActivityIndicator';
+// export { ActivityIndicator } from './CoreActivityIndicator';
+// export default ActivityIndicator;
+
+import { ActivityIndicator } from './ActivityIndicator.mobile';
+export { ActivityIndicator } from './ActivityIndicator.mobile';
 export default ActivityIndicator;
